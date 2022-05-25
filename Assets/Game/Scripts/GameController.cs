4f9b2921cd3a92b7157805162fbc0966e60ef976
ryan ramos <ryan.vramos@gmail.com>
2022-05-25 11:06:53 +0800

    initial commit

---
+++ Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gotoandplay
+{
+    public class GameController : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+}
