7599683d08444cfb75c05ebb96fc18a97a560094
ryan ramos <ryan.vramos@gmail.com>
2022-05-25 11:55:15 +0800

    updated source

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -8,7 +8,18 @@ namespace gotoandplay
     {
         private void Start()
         {
-            
+            // Test tower of hanoi logic
+            //TOH(3, 1, 2, 3);
+        }
+
+        public void TOH(int n, int A, int B, int C)
+        {
+            if(n > 0)
+            {
+                TOH(n - 1, A, C, B);
+                Debug.Log(string.Format("disc moved from {0} to {1}", A, C));
+                TOH(n - 1, B, A, C);
+            }
         }
     }
 }
---
+++ Assets/Game/Scripts/TowerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gotoandplay
+{
+    public class TowerController : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+}
